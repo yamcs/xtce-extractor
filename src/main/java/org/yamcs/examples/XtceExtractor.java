6e648b08dac9802d8cb29b292735d509a3b3cd70
Nicolae Mihalache <nicolae.mihalache@spaceapplications.com>
2018-01-09 17:47:26 +0100

    updated to work with yamcs 3.4.0

--- src/main/java/org/yamcs/examples/XtceExtractor.java
+++ src/main/java/org/yamcs/examples/XtceExtractor.java
@@ -1,9 +1,8 @@
 package org.yamcs.examples;
 
-import java.nio.ByteBuffer;
-
 import org.yamcs.parameter.ParameterValue;
 import org.yamcs.parameter.ParameterValueList;
+import org.yamcs.utils.BitBuffer;
 import org.yamcs.utils.TimeEncoding;
 import org.yamcs.xtce.XtceDb;
 import org.yamcs.xtceproc.XtceDbFactory;
@@ -18,7 +17,7 @@ public class XtceExtractor {
     
     public static void main( String[] args ) {
         TimeEncoding.setUp();
-        XtceDb xtcedb = XtceDbFactory.createInstance(XTCE_DB_NAME);
+        XtceDb xtcedb = XtceDbFactory.createInstanceByConfig(XTCE_DB_NAME);
         XtceTmExtractor tmExtractor=new XtceTmExtractor(xtcedb);
         
         //instruct the tm extractor to provide all parameters
@@ -30,7 +29,7 @@ public class XtceExtractor {
                                 0x00, 0x10, 0x02, 0x00, 0x30, 0x04}; // 4 Samples
         long t = TimeEncoding.getWallclockTime();
         
-        tmExtractor.processPacket(ByteBuffer.wrap(p1), t, t);
+        tmExtractor.processPacket(new BitBuffer(p1), t, t);
         ParameterValueList pvl = tmExtractor.getParameterResult();
         for(ParameterValue pv: pvl) {
             System.out.println(pv.getParameter().getName()+":{ eng: "+pv.getEngValue()+", raw: "+pv.getRawValue()+"}");
